d68569060d6d42f9fe78b5dcb94ab449beb1200c
Justin Lupica <justinlupica@gmail.com>
2020-07-14 00:54:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,24 @@
 def reverse_each_word(string)
-  split_string = string.split("")
-  reversed = []
-  split_string.each { reversed << split_string.shift }
-  reversed.join
+
+  original_array = string.split(" ")
+  array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+  array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  test_array = []
+  array.collect do|string| 
+    test_array << string.reverse
+  return_array = []
+  array.collect do|string|
+    return_array << string.reverse
+  end
+  test_array.join(" ")
+  return_array.join(" ")
 end
